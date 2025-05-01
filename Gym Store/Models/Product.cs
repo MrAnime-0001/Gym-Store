f47d67ff147b46d2b3b345f99a06130f8b029c8d
yutti <yutti@YUTTI_CHYLONG>
2025-05-02 11:45:11 +1200

    Fixed an issue where the Model.Product could not be found by other cshtml

--- Gym Store/Models/Product.cs
+++ Gym Store/Models/Product.cs
@@ -1,30 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
 
-public class Product
+namespace Gym_Store.Models
 {
-    [Key]
-    public int Id { get; set; }
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
 
-    [Required]
-    [MaxLength(100)]
-    [DisplayName("Product Name")]
-    public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
 
-    [Required]
-    [MaxLength(50)]
-    [DisplayName("Product Type")]
-    public string Type { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [DisplayName("Product Type")]
+        public string Type { get; set; }
 
-    [Required]
-    [Range(0.01, 10000, ErrorMessage = "Please enter a valid price between 0.01 and 10000")]
-    [DisplayName("Price (NZD)")]
-    public decimal Price { get; set; }
+        [Required]
+        [Range(0.01, 10000, ErrorMessage = "Please enter a valid price between 0.01 and 10000")]
+        [DisplayName("Price (NZD)")]
+        public decimal Price { get; set; }
 
-    [Required]
-    public string Quantity { get; set; }
+        [Required]
+        public string Quantity { get; set; }
 
-    [DisplayName("Image URL")]
-    [Url(ErrorMessage = "Please enter a valid URL")]
-    public string? ImageUrl { get; set; }
+        [DisplayName("Image URL")]
+        [Url(ErrorMessage = "Please enter a valid URL")]
+        public string? ImageUrl { get; set; }
+    }
 }
